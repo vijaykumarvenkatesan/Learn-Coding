132f1817130d26d3f64badb760e999930f877077
vijay <vv16062004@gmail.com>
2025-02-10 21:04:21 +0530

    adding the three basic array program

--- Basic Recursion/src/print1ToN.java
+++ Basic Recursion/src/print1ToN.java
@@ -6,9 +6,16 @@ public class print1ToN {
         System.out.println(i);
         print(i+1,n);
     }
+    public static void printno1(int n){
+        if(n==0) return;
+        System.out.println(n);
+        printno1(--n);
+    }
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        print(1,n);
+        // print(1,n);
+        printno1(n);
     }
+
 }
