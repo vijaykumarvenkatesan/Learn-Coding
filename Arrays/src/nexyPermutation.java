ccf04ace9fe5b9629876440213f0549f76de4255
vijay <vv16062004@gmail.com>
2025-03-27 12:48:56 +0530

    nextPermutation

---
+++ Arrays/src/nexyPermutation.java
@@ -0,0 +1,43 @@
+public class nexyPermutation {
+    public static void main(String[] args) {
+        int[] nums;
+        int index = -1;
+        int n = nums.length;
+        for(int i = n-2; i >=0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                System.out.println(i);
+                break;
+            }
+        }
+        if(index == -1){
+            int i =0;
+            int j = n-1;
+            while(i < j){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+            return;
+        }
+        for(int i = n-1 ; i > index; i--){
+            if(nums[i] > nums[index]){
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+                break;
+            }
+        }
+        int i = index+1;
+        int j =n-1;
+        while (i < j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
---
+++ leetcode/src/_1859_sort_the_sentence.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+import static jdk.internal.org.jline.utils.Colors.s;
+
+public class _1859_sort_the_sentence {
+    public static void main(String[] args) {
+        String  s = "";
+        String str[] = s.split(" ");
+        HashMap <Integer, String> encode = new HashMap<>();
+
+        for(String w : str){
+            int lastValue = w.charAt(w.length()-1) - '0';
+            encode.put(lastValue, w.substring(0,w.length()-1));
+        }
+
+        StringBuilder word = new StringBuilder();
+        for(int i=1; i<=str.length;i++){
+            word.append(encode.get(i)+" ");
+        }
+//        return word.toString().trim();
+    }
+}
